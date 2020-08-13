451bb25ab9d52e39826f5fd6e13d2ffa73518f7f
Chad Rutherford <chad_rutherford@outlook.com>
2020-08-13 11:36:43 -0400

    Finished Project 2

--- Project2/Project2 WatchKit Extension/InterfaceController.swift
+++ Project2/Project2 WatchKit Extension/InterfaceController.swift
@@ -18,12 +18,20 @@ class InterfaceController: WKInterfaceController {
 	@IBOutlet weak var scissors: WKInterfaceButton!
 	@IBOutlet weak var levelCounter: WKInterfaceLabel!
 	@IBOutlet weak var timer: WKInterfaceTimer!
+	@IBOutlet var result: WKInterfaceLabel!
 	
 	var allMoves = ["rock", "paper", "scissors"]
 	var shouldWin = true
 	var level = 1
 	
 	private func newLevel() {
+		if level == 21 {
+			result.setHidden(false)
+			question.setHidden(true)
+			answers.setHidden(true)
+			timer.stop()
+			return
+		}
 		levelCounter.setText("\(level)/20")
 		if Bool.random() {
 			setTitle("Win!")
@@ -34,6 +42,17 @@ class InterfaceController: WKInterfaceController {
 		}
 		allMoves.shuffle()
 		question.setImage(UIImage(named: allMoves[0]))
+	}
+	
+	private func check(for answer: String) {
+		if allMoves[0] == answer {
+			level += 1
+			newLevel()
+		} else {
+			level -= 1
+			if level < 1 { level = 1 }
+			newLevel()
+		}
 	}
 	
 	override func awake(withContext context: Any?) {
@@ -54,14 +73,26 @@ class InterfaceController: WKInterfaceController {
     }
 	
 	@IBAction func rockTapped() {
-		
+		if shouldWin {
+			check(for: "scissors")
+		} else {
+			check(for: "paper")
+		}
 	}
 	
 	@IBAction func paperTapped() {
-		
+		if shouldWin {
+			check(for: "rock")
+		} else {
+			check(for: "scissors")
+		}
 	}
 	
 	@IBAction func scissorsTapped() {
-		
+		if shouldWin {
+			check(for: "paper")
+		} else {
+			check(for: "rock")
+		}
 	}
 }
