f41e9bae2e04661c7cc2a4421bb1ddf48eb792a1
Chad Rutherford <chad_rutherford@outlook.com>
2020-08-13 11:21:57 -0400

    Created initial UI and functionality

--- Project2/Project2 WatchKit Extension/InterfaceController.swift
+++ Project2/Project2 WatchKit Extension/InterfaceController.swift
@@ -11,8 +11,38 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
-    override func awake(withContext context: Any?) {
-        // Configure interface objects here.
+	@IBOutlet weak var question: WKInterfaceImage!
+	@IBOutlet weak var answers: WKInterfaceGroup!
+	@IBOutlet weak var rock: WKInterfaceButton!
+	@IBOutlet weak var paper: WKInterfaceButton!
+	@IBOutlet weak var scissors: WKInterfaceButton!
+	@IBOutlet weak var levelCounter: WKInterfaceLabel!
+	@IBOutlet weak var timer: WKInterfaceTimer!
+	
+	var allMoves = ["rock", "paper", "scissors"]
+	var shouldWin = true
+	var level = 1
+	
+	private func newLevel() {
+		levelCounter.setText("\(level)/20")
+		if Bool.random() {
+			setTitle("Win!")
+			shouldWin = true
+		} else {
+			setTitle("Lose!")
+			shouldWin = false
+		}
+		allMoves.shuffle()
+		question.setImage(UIImage(named: allMoves[0]))
+	}
+	
+	override func awake(withContext context: Any?) {
+		super.awake(withContext: context)
+		rock.setBackgroundImage(UIImage(named: "rock"))
+		paper.setBackgroundImage(UIImage(named: "paper"))
+		scissors.setBackgroundImage(UIImage(named: "scissors"))
+		timer.start()
+		newLevel()
     }
     
     override func willActivate() {
@@ -22,5 +52,16 @@ class InterfaceController: WKInterfaceController {
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
     }
-
+	
+	@IBAction func rockTapped() {
+		
+	}
+	
+	@IBAction func paperTapped() {
+		
+	}
+	
+	@IBAction func scissorsTapped() {
+		
+	}
 }
