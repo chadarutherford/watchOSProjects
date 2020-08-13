ff2ab9cc3b6b15bce20956b343037ffea73c9c53
Chad Rutherford <chad_rutherford@outlook.com>
2020-08-13 15:12:34 -0400

    Added Currencies and populated WKPicker

--- Project3/Project3 WatchKit Extension/InterfaceController.swift
+++ Project3/Project3 WatchKit Extension/InterfaceController.swift
@@ -10,6 +10,18 @@ import Foundation
 
 
 // 9f3c490c82ee4e40832bfd05b9db84bf
+enum Currencies: String, CaseIterable {
+	case usd = "USD"
+	case aud = "AUD"
+	case cad = "CAD"
+	case chf = "CHF"
+	case cny = "CNY"
+	case eur = "EUR"
+	case gbp = "GBP"
+	case hkd = "HKD"
+	case jpy = "JPY"
+	case sgd = "SGD"
+}
 
 class InterfaceController: WKInterfaceController {
 
@@ -17,8 +29,18 @@ class InterfaceController: WKInterfaceController {
 	@IBOutlet var amountSlider: WKInterfaceSlider!
 	@IBOutlet var currencyPicker: WKInterfacePicker!
 	
+	static let defaultCurrencies = ["USD", "EUR"]
+	var currentCurrency: Currencies = .usd
+	var currentAmount = 500
+	
 	override func awake(withContext context: Any?) {
-        // Configure interface objects here.
+        var items = [WKPickerItem]()
+		for currency in Currencies.allCases {
+			let item = WKPickerItem()
+			item.title = currency.rawValue
+			items.append(item)
+		}
+		currencyPicker.setItems(items)
     }
     
     override func willActivate() {
@@ -30,7 +52,8 @@ class InterfaceController: WKInterfaceController {
     }
 	
 	@IBAction func amountChanged(_ value: Float) {
-		
+		currentAmount = Int(value)
+		amountLabel.setText(String(currentAmount))
 	}
 	
 	@IBAction func convertTapped() {
@@ -38,6 +61,6 @@ class InterfaceController: WKInterfaceController {
 	}
 	
 	@IBAction func baseCurrencyChanged(_ value: Int) {
-		
+		currentCurrency = Currencies.allCases[value]
 	}
 }
