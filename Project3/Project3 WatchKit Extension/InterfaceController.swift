807bd3c11a47cc4b0057b894779dfbd5d6e0c13a
Chad Rutherford <chad_rutherford@outlook.com>
2020-08-13 11:51:24 -0400

    Created UI and wired up IBOutlets and IBActions

--- Project3/Project3 WatchKit Extension/InterfaceController.swift
+++ Project3/Project3 WatchKit Extension/InterfaceController.swift
@@ -9,9 +9,15 @@ import WatchKit
 import Foundation
 
 
+// 9f3c490c82ee4e40832bfd05b9db84bf
+
 class InterfaceController: WKInterfaceController {
 
-    override func awake(withContext context: Any?) {
+	@IBOutlet var amountLabel: WKInterfaceLabel!
+	@IBOutlet var amountSlider: WKInterfaceSlider!
+	@IBOutlet var currencyPicker: WKInterfacePicker!
+	
+	override func awake(withContext context: Any?) {
         // Configure interface objects here.
     }
     
@@ -22,5 +28,16 @@ class InterfaceController: WKInterfaceController {
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
     }
-
+	
+	@IBAction func amountChanged(_ value: Float) {
+		
+	}
+	
+	@IBAction func convertTapped() {
+		
+	}
+	
+	@IBAction func baseCurrencyChanged(_ value: Int) {
+		
+	}
 }
