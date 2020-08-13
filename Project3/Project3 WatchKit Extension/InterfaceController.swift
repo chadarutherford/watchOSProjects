32e8a062c0375412907541e6f666b729facfe146
Chad Rutherford <chad_rutherford@outlook.com>
2020-08-13 11:37:42 -0400

    Added Project 3 - WatchFX

---
+++ Project3/Project3 WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  Project3 WatchKit Extension
+//
+//  Created by Chad Rutherford on 8/13/20.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
